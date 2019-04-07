f31c28890a27dcb3578bca87eb13946711b8e307
lsq <yan647@aliyun.com>
2019-04-07 20:58:19 +0800

    仿照官网的井字棋游戏：通过props传参数

---
+++ my-app/src/index.js
@@ -0,0 +1,74 @@
+/**
+ * Created by lsq on 2019/4/7.
+ * 仿照官网的井字棋游戏
+ */
+
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component { //组件类型
+    render() {//返回嵌套结构的视图，是个react元素
+        return (
+            <button className="square">
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />; //value 是个名字为value的props，用来传参数
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
