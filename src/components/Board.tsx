18ff5527874d651c0ee54b7017606d86369fa1b1
yan647 <yan647@aliyun.com>
2023-03-28 20:02:01 +0800

    feat: Square组件

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,11 +1,13 @@
+import Square from "./Square";
+
 export default function Board() {
-  let columns = new Array(3).fill([]).map((item, index) => [index * 3 + 1, index * 3 + 2, index * 3 + 3]);
+  let columns = new Array(3).fill([]).map(() => new Array(3).fill(''));
   return (
     <>
       {
         columns.map((column, colIndex) => {
           return (<div className="board-row" key={colIndex}>
-            {column.map((row, rowIndex) => (<button className="square" key={rowIndex}>{row}</button>))}
+            {column.map(() => (<Square />))}
           </div>)
         })
       }
---
+++ src/components/Square.tsx
@@ -0,0 +1,3 @@
+export default function Square(){
+  return <button className="square" >1</button>;
+}
