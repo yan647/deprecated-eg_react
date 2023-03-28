dbcfdb3178e65b2e2800fcf314a742524e23d393
yan647 <yan647@aliyun.com>
2023-03-28 19:42:44 +0800

    feat: 使用for循环替换

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,21 +1,14 @@
 export default function Board() {
+  let columns = new Array(3).fill([]).map((item, index) => [index * 3 + 1, index * 3 + 2, index * 3 + 3]);
   return (
     <>
-      <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
-      </div>
-      <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
-      </div>
-      <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
-      </div>
+      {
+        columns.map((column, colIndex) => {
+          return (<div className="board-row" key={colIndex}>
+            {column.map((row, rowIndex) => (<button className="square" key={rowIndex}>{row}</button>))}
+          </div>)
+        })
+      }
     </>
   );
 }
